340d316b9c70f22c2d3932796836fb1186c36aa3
eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2019-09-25 11:51:40 -0700

    created all the routers

---
+++ backend/controllers/stuff.js
@@ -0,0 +1,24 @@
+const Thing = require('../models/thing');
+
+exports.creatThing = (req, res, next) => {
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+}
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,44 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const stuffController = require("../controllers/stuff");
+
 const Thing = require('../models/thing');
 
-router.post('/', (req, res, next) => {
-  const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  thing.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
+router.post('/', stuffController.creatThing);
 
-// router.get('/', (req, res, next) => {
-//   Thing.find().then(
-//     (things) => {
-//       res.status(200).json(things);
-//     }
-//   ).catch(
-//     (error) => {
-//       res.status(400).json({
-//         error: error
-//       });
-//     }
-//   );
-// });
+
 
 router.get('/:id', (req, res, next) => {
   Thing.findOne({
